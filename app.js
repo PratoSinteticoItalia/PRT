f931fcc8fd11ab2d01f9a196bac2f44ff696a0c6
PratoSinteticoItalia <info@pratosinteticoitalia.it>
2026-05-07 13:19:10 +0200

    Update Abete reseller price

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_SHELL_VERSION = "20260507-coverage-map-calibration-132";
+const APP_SHELL_VERSION = "20260507-abete-rivenditore-price-133";
 const APP_SHELL_VERSION_STORAGE_KEY = "psi-shell-version";
 const RDF_PORTAL_URL = "https://rdf.spedisci.online/login";
 const crews = ["Alpha", "Beta", "Delta"];
